34642e4f5022e8a144265db434cdf9253d7e013c
aomathwift <a.okw1011@gmail.com>
2021-08-30 23:38:26 +0900

    Initial Commit

---
+++ SandboxKit/SandboxKit.h
@@ -0,0 +1,18 @@
+//
+//  SandboxKit.h
+//  SandboxKit
+//
+//  Created by aoi-okawa on 2021/08/30.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SandboxKit.
+FOUNDATION_EXPORT double SandboxKitVersionNumber;
+
+//! Project version string for SandboxKit.
+FOUNDATION_EXPORT const unsigned char SandboxKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SandboxKit/PublicHeader.h>
+
+
